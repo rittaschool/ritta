2cb95216a36ba60ed387ad2d21c4a351cb4f90b2
Raikas <roni.aikas@gmail.com>
2021-04-18 19:47:38 +0300

    Added placeholders to navbar. WIP: Notifications with websockets

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const passport = require('passport');
 const OpinsysStrategy = require('passport-opinsys');
 const moment = require('moment');
 const LocalStrategy = require('passport-local');
+const WebSocket = require('ws');
 
 require('dotenv').config();
 
@@ -130,6 +131,23 @@ passport.deserializeUser((id, done) => {
   });
 });
 
+// Notification server.
+
+const wss = new WebSocket.Server({
+  noServer: true,
+});
+
+const userNotificationMap = {};
+
+wss.on('connection', (ws, request, client) => {
+  ws.on('message', (message) => {
+    const json = JSON.parse(message);
+    console.log(`youve got mail "${message}" from ${client}`);
+  });
+
+  ws.send(JSON.stringify({ status: 1, message: 'Connected.' }));
+});
+
 /*
  *
  * WEB
@@ -189,6 +207,7 @@ app.get('/', isLoggedIn, (req, res) => {
     school: config.school,
     username: req.user.username,
     user: req.user,
+    notificationID: utils.encrypt(req.user.id),
   });
 });
 
@@ -221,6 +240,7 @@ app.get('/messages', isLoggedIn, (req, res) => {
             moment,
             sent: sent.length,
             archive: archive.length,
+            notificationID: utils.encrypt(req.user.id),
           });
         });
       });
@@ -257,6 +277,7 @@ app.get('/messages/sent', isLoggedIn, (req, res) => {
             moment,
             outbox: outbox.length,
             archive: archive.length,
+            notificationID: utils.encrypt(req.user.id),
           });
         });
       });
@@ -293,6 +314,7 @@ app.get('/messages/archive', isLoggedIn, (req, res) => {
             moment,
             outbox: outbox.length,
             sent: sent.length,
+            notificationID: utils.encrypt(req.user.id),
           });
         });
       });
@@ -308,6 +330,7 @@ app.get('/messages/send', isLoggedIn, (req, res) => {
     username: req.user.username,
     user: req.user,
     csrfToken: req.csrfToken(),
+    notificationID: utils.encrypt(req.user.id),
   });
 });
 
@@ -338,7 +361,7 @@ app.post('/messages/send', isLoggedIn, async (req, res, next) => {
     database.newThread(req.user.id, req.body.title, req.body.content, recipients).then((thread) => {
       if (!thread) {
         const error = new Error('Recipient list empty');
-        error.code = 403;
+        error.code = 400;
         next(error);
         return;
       }
@@ -357,7 +380,7 @@ app.get('/messages/:messageid', isLoggedIn, (req, res, next) => {
     }
     if (
       thread.sender === req.user.id
-      || thread.recipients.filter((r) => r.userId === req.user.id)
+      || thread.recipients.find((r) => r.userId === req.user.id)
     ) {
       const newThread = thread.toObject();
       database.getUserDataById(thread.sender).then((user) => {
@@ -399,6 +422,7 @@ app.get('/messages/:messageid', isLoggedIn, (req, res, next) => {
             decrypt: utils.decrypt,
             messages,
             csrfToken: req.csrfToken(),
+            notificationID: utils.encrypt(req.user.id),
           });
         });
       });
@@ -420,7 +444,7 @@ app.post('/messages/:messageid/reply', isLoggedIn, (req, res, next) => {
     }
     if (
       thread.sender === req.user.id
-      || thread.recipients.filter((r) => r.userId === req.user.id)
+      || thread.recipients.find((r) => r.userId === req.user.id)
     ) {
       if (!req.body.content) {
         const error = new Error('Content-parameter missing');
@@ -452,6 +476,7 @@ app.get('/logout', isLoggedIn, (req, res) => {
   req.session.destroy(() => {});
   setTimeout(() => { res.redirect('/account/login?loggedout=true'); }, 200);
 });
+
 app.get('/account/:action', (req, res) => {
   switch (req.params.action.toLowerCase()) {
     case 'createuser':
@@ -503,6 +528,20 @@ app.post('/api/:action', (req, res) => {
       break;
   }
 });
+app.get('/api/notification', (req, res) => {
+  if (req.query.user) {
+    if (!userNotificationMap[req.query.user]) {
+      res.send('lol222');
+      return;
+    }
+    userNotificationMap[req.query.user].forEach((ws) => {
+      ws.send(req.query.notification);
+    });
+    res.send('sent');
+  } else {
+    res.send('lol');
+  }
+});
 app.get('/api/calendar/:userid', (req, res) => {
   req.rateLimitThis = true;
   const value = utils.createCalendar([{
@@ -536,8 +575,11 @@ app.get('/api/calendar/:userid', (req, res) => {
   });
 });
 app.all('*', (req, res) => {
-  res.status(404).render(`${__dirname}/web/error/404.ejs`, {
+  const error = new Error('Not found');
+  error.code = 404;
+  res.status(404).render(`${__dirname}/web/error.ejs`, {
     lang,
+    error,
     school: config.school,
     version: packageJSON.version,
   });
@@ -546,7 +588,7 @@ app.all('*', (req, res) => {
 // eslint-disable-next-line no-unused-vars
 app.use((error, req, res, next) => {
   const status = error.code || 500;
-  res.status(status).render(`${__dirname}/web/error/500.ejs`, {
+  res.status(status).render(`${__dirname}/web/error.ejs`, {
     error,
     lang,
     school: config.school,
@@ -564,6 +606,40 @@ if (config.ssl.enabled) {
     cert: certificate,
   }, app).listen(config.website.port, () => {
     console.log(`Website is now running on port ${config.website.port}`);
+  });
+  server.on('upgrade', (request, socket, head) => {
+    if (request.headers.upgrade !== 'websocket') {
+      return;
+    }
+    const query = request.url.slice(request.url.indexOf('?') + 1)
+      .split('&')
+      .reduce((a, c) => {
+        const [key, value] = c.split('=');
+        const b = a;
+        b[key] = value;
+        return b;
+      }, {});
+    if (!query.id) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    }
+    // validate user
+    database.getUserDataById(utils.decrypt(query.id)).then((user) => {
+      if (user) {
+        wss.handleUpgrade(request, socket, head, (ws) => {
+          if (userNotificationMap[user.id]) {
+            userNotificationMap[user.id] = userNotificationMap[user.id].push(ws);
+          } else {
+            userNotificationMap[user.id] = [ws];
+          }
+          wss.emit('connection', ws, request, user);
+        });
+      } else {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+        socket.destroy();
+      }
+    });
   });
 } else {
   server = app.listen(config.website.port, () => {
