f0e16251543b7d3dd53c3ecde1efc8b96a86678c
raikasdev <fanipostiraikas@gmail.com>
2021-05-13 16:29:16 +0300

    Flash

--- src/index.js
+++ src/index.js
@@ -553,7 +553,7 @@ app.get('/account/:action', (req, res) => {
       } else if (req.query.invalid_mfa) {
         error = 'Virheellinen kaksivaiheisen tunnistautumisen koodi';
       }
-      error = req.session.messages || error;
+      error = req.session.messages[0] || error;
       res.render(`${__dirname}/web/loginpage.ejs`, {
         lang, school: config.school, opinsys: config.opinsys, error, csrfToken: req.csrfToken(),
       });
