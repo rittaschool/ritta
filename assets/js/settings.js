8d4538ef1b882228b38078a5460770bea0fea8a3
Raikas <roni.aikas@gmail.com>
2021-05-18 23:18:58 +0300

    f

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -14,7 +14,6 @@ function enableMFA() {
     enable.style.display = '';
     enable_b.addEventListener('click', () => {
         enable_b.classList.add('btn-progress');
-        console.log(document.getElementById('csrf-token').getAttribute('content'));
         axios.post('/account/mfa', { 
             _csrf: document.getElementById('csrf-token').getAttribute('content')
         }, { 
@@ -45,7 +44,8 @@ function disableMFA() {
     disable_b.addEventListener('click', () => {
         axios.delete('/account/mfa', {
             data: {
-                code: disable_code.value
+                code: disable_code.value,
+                _csrf: document.getElementById('csrf-token').getAttribute('content')
             },
             withCredentials: true, 
             headers: {
