fc8081af31f49487e97fa4bb5e4ba6c51e10360e
Raikas <roni.aikas@gmail.com>
2021-05-21 16:38:02 +0300

    f

--- src/features/dev.js
+++ src/features/dev.js
@@ -9,12 +9,12 @@ const router = new express.Router();
 api.add('/dev', router);
 
 // Development purposes, will be removed in 1.0
-router.post('/eval', isAllowedToAccess([100]), (req, res) => {
+router.post('/eval', (req, res, next) => isAllowedToAccess(req, res, next, [100]), (req, res) => {
   if (!req.body.command) {
     return res.status(400).json({ response: '', error: true });
   }
-  // eslint-disable-next-line no-eval
   try {
+    // eslint-disable-next-line no-eval
     res.json({ response: eval(req.body.command), error: false });
   } catch (e) {
     return res.status(500).json({ response: e, error: true });
