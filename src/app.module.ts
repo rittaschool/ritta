2c340540240da9546d2314e9b93d021e044d7dcf
kymppi <me@midka.dev>
2021-09-24 19:20:24 +0300

    Added configuration files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from 'config/configuration';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo/ritta-server')],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/ritta-server'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
