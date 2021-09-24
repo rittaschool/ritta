5149ff48d3228611616c649dea381deb1ab46047
kymppi <me@midka.dev>
2021-09-24 19:14:24 +0300

    MongoDB initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://mongo/ritta-server')],
   controllers: [AppController],
   providers: [AppService],
 })
