e0c25a26bea774626982352dac998ef9a3065e9f
kymppi <me@midka.dev>
2021-09-25 22:31:22 +0300

    Added user filtering, so anybode dont get secrets

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,8 +26,8 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,10 +3,15 @@ import { Injectable } from '@nestjs/common';
 import { Cryptor } from 'src/utils/encryption';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { RandomString } from 'src/utils/randomString';
+
+export type FilteredUser = Omit<
+  User,
+  'password' | 'secret' | 'mfaBackup' | 'mfaSecret' | 'yubiPIN' | 'yubikeyId'
+>;
 
 @Injectable()
 export class UsersService {
@@ -37,19 +42,68 @@ export class UsersService {
     return record;
   }
 
-  findAll() {
-    return this.userModel.find().exec();
+  async findAll(filter = true): Promise<FilteredUser[] | User[]> {
+    const userDocs = await this.userModel.find().exec();
+
+    if (filter) {
+      const users: FilteredUser[] = [];
+
+      userDocs.map(async (user) => {
+        const filUser = await this.filterUser(user);
+
+        users.push(filUser);
+      });
+
+      return users;
+    }
+
+    return userDocs.map((user) => user.toObject());
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string, filter = true) {
+    const user = await this.userModel.findById(id).exec();
+    const filteredUser = user.toObject();
+
+    if (filter) {
+      delete filteredUser.password;
+      delete filteredUser.secret;
+      delete filteredUser.mfaBackup;
+      delete filteredUser.mfaSecret;
+      delete filteredUser.yubiPIN;
+      delete filteredUser.yubikeyId;
+      delete filteredUser.__v;
+
+      filteredUser['id'] = filteredUser['_id'];
+
+      delete filteredUser._id;
+    }
+
+    return filteredUser;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async filterUser(user: UserDocument): Promise<FilteredUser> {
+    const filteredUser = user.toObject();
+
+    delete filteredUser.password;
+    delete filteredUser.secret;
+    delete filteredUser.mfaBackup;
+    delete filteredUser.mfaSecret;
+    delete filteredUser.yubiPIN;
+    delete filteredUser.yubikeyId;
+    delete filteredUser.__v;
+
+    filteredUser['id'] = filteredUser['_id'];
+
+    delete filteredUser._id;
+
+    return filteredUser;
   }
 }
