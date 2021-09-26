fed01f8504f29015d1aedca101b20d99f29d5e70
kymppi <me@midka.dev>
2021-09-26 18:41:14 +0300

    fix: users.service.tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,4 +1,8 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { RandomString } from '../utils/randomString';
+import { Cryptor } from '../utils/encryption';
+import { User } from './schemas/user.schema';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -6,7 +10,28 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User.name),
+          useValue: {},
+        },
+        {
+          provide: Cryptor,
+          useValue: () => {
+            encrypt: (text: string) =>
+              `ENCRYPTED||ENCRYPTED||${text}||ENCRYPTED||ENCRYPTED`;
+            decrypt: (text: string) =>
+              text.split('||').splice(0, 2).splice(3, 2);
+          },
+        }, 
+        {
+          provide: RandomString,
+          useValue: {
+            generate: (_length: number) => 'heresareallylongandrandomstring'
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
