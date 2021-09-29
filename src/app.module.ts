ad9a01b27eb910b46b39c9603b2f08a1ee6a4089
kymppi <me@midka.dev>
2021-09-29 18:04:41 +0300

    chore(move): move school schema from users to school

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { UtilsModule } from './utils/utils.module';
 import { AuthModule } from './auth/auth.module';
+import { SchoolsModule } from './schools/schools.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -21,6 +22,7 @@ import configuration from './config/configuration';
     UsersModule,
     UtilsModule,
     AuthModule,
+    SchoolsModule,
   ],
 })
 export class AppModule {}
---
+++ src/schools/schools.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { SchoolsService } from './schools.service';
+import { CreateSchoolDto } from './dto/create-school.dto';
+import { UpdateSchoolDto } from './dto/update-school.dto';
+
+@Controller('schools')
+export class SchoolsController {
+  constructor(private readonly schoolsService: SchoolsService) {}
+
+  @Post()
+  create(@Body() createSchoolDto: CreateSchoolDto) {
+    return this.schoolsService.create(createSchoolDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.schoolsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.schoolsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto) {
+    return this.schoolsService.update(+id, updateSchoolDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.schoolsService.remove(+id);
+  }
+}
