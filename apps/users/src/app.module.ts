bca68a53006904f5840558994f62b32531a598fb
kymppi <me@midka.dev>
2021-10-21 21:04:03 +0300

    fix: remove unused files

--- apps/users/src/app.module.ts
+++ apps/users/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { validate } from './validation/env.validation';
 import { UsersModule } from './users/users.module';
