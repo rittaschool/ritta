2b60f5f6a180bcfa9fd6b1a270c925902c2274e0
kymppi <me@midka.dev>
2021-10-23 12:44:01 +0300

    fix: add users.repository

--- apps/users/src/users/users.module.ts
+++ apps/users/src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
---
+++ apps/users/src/users/users.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entities/user.entity';
+
+@Injectable()
+export class UsersRepository {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<User> {
+    return this.userModel.findById(id).exec();
+  }
+
+  async create(user: User): Promise<User> {
+    const createdUser = new this.userModel(user);
+    return createdUser.save();
+  }
+
+  async update(id: string, user: User): Promise<User> {
+    return this.userModel.findByIdAndUpdate(id, user).exec();
+  }
+
+  async delete(id: string): Promise<User> {
+    return this.userModel.findByIdAndRemove(id).exec();
+  }
+}
