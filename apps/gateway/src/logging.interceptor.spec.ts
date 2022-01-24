34e48a5c3f444d4206bab26954350c937c430933
kymppi <me@midka.dev>
2022-01-24 17:47:11 +0200

    fix: gateway tests

--- apps/gateway/src/logging.interceptor.spec.ts
+++ apps/gateway/src/logging.interceptor.spec.ts
@@ -1,7 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { LoggingInterceptor } from './logging.interceptor';
 
 describe('LoggingInterceptor', () => {
+  let interceptor: LoggingInterceptor;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: 'LOGGING_INTERCEPTOR',
+          useClass: LoggingInterceptor,
+        },
+        {
+          provide: 'LOGGER',
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    interceptor = module.get<LoggingInterceptor>('LOGGING_INTERCEPTOR');
+  });
+
   it('should be defined', () => {
-    expect(new LoggingInterceptor()).toBeDefined();
+    expect(interceptor).toBeDefined();
   });
 });
