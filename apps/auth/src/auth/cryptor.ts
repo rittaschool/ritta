c6a4e9a5212aa014aa8825d626cb968be8cfbb57
raikasdev <roni.aikas@gmail.com>
2022-01-30 19:56:14 +0200

    feat: switch to crypto

--- apps/auth/src/auth/cryptor.ts
+++ apps/auth/src/auth/cryptor.ts
@@ -1,11 +1,11 @@
-import { compare, hash } from 'bcrypt';
+import crypto from 'crypto';
 
 const hashPassword = (password: string) => {
-  return hash(password, 15);
+  return crypto.createHash('sha512').update(password).digest('hex');
 };
 
 const verifyPassword = async (password: string, hash: string) => {
-  return await compare(password, hash);
+  return hashPassword(password) === hash;
 };
 
 export default {
