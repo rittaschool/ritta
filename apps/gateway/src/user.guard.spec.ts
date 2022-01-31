a8e940de309e73a3d7e5c5ba40009cf7a998942a
kymppi <me@midka.dev>
2022-01-31 11:49:43 +0200

    fix: tests

--- apps/gateway/src/user.guard.spec.ts
+++ apps/gateway/src/user.guard.spec.ts
@@ -1,7 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { UserGuard } from './user.guard';
 
 describe('UserGuard', () => {
+  let guard: UserGuard;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserGuard,
+        { provide: 'USERS_SERVICE', useValue: {} },
+        { provide: 'LOGGER', useValue: {} },
+        { provide: 'TOKENIZER', useValue: {} },
+      ],
+    }).compile();
+
+    guard = module.get<UserGuard>(UserGuard);
+  });
+
   it('should be defined', () => {
-    expect(new UserGuard()).toBeDefined();
+    expect(guard).toBeDefined();
   });
 });
