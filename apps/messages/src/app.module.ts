7ec6fabeb737b6fc86fa9317813fe3ef34e31c18
raikasdev <roni.aikas@gmail.com>
2022-03-26 01:30:45 +0200

    fix: add todo

--- apps/messages/src/app.module.ts
+++ apps/messages/src/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { validate } from './validation/env.validation';
 import { MessagesModule } from './messages/messages.module';
 import { AnnouncementsModule } from './announcements/announcements.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate,
     }),
+    CommonModule,
     MessagesModule,
     AnnouncementsModule,
   ],
--- apps/users/src/users/encrypt.ts
+++ apps/users/src/users/encrypt.ts
@@ -1,8 +1,8 @@
 import * as argon2 from 'argon2';
 
 const encodePassword = async (password: string): Promise<string> => {
-  console.log('hashing');
-  console.log(await argon2.hash(password, { saltLength: 8, timeCost: 3 }));
+  console.log('hashing'); // TODO: remove debug
+  console.log(await argon2.hash(password, { saltLength: 8, timeCost: 3 })); // TODO: remove debug
   return await argon2.hash(password, { saltLength: 16, timeCost: 3 });
 };
 
