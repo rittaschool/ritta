a943b88db785af2e1a8af5ff12387be01353d8af
Midka <me@midka.dev>
2022-05-13 15:52:42 +0300

    feat(rename): remove extra dot from permissions.guard..ts

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,27 +1,27 @@
-import { TerminusModule } from '@nestjs/terminus';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
 import { APP_GUARD } from '@nestjs/core';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { CommonModule } from './common/common.module';
-import { CustomContext } from './graphql-ctx';
-import { UsersModule } from './users/users.module';
-import { validate } from './validation/env.validation';
 import { ChallengeModule } from './challenge/challenge.module';
-import { UserGuard } from './guards/user.guard';
-import { RidGuard } from './guards/rid.guard';
-import { Tokenizer } from './validation/tokenizer';
-import { GqlUserGuard } from './gql-user.guard';
-import { HealthController } from './health/health.controller';
-import { PermissionsGuard } from './permissions.guard.';
-import { MicroserviceHealthIndicator } from './health/rmq.health';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CommonModule } from './common/common.module';
 import { Fido2Controller } from './fido2/fido2.controller';
 import { Fido2Module } from './fido2/fido2.module';
+import { GqlUserGuard } from './gql-user.guard';
+import { CustomContext } from './graphql-ctx';
+import { RidGuard } from './guards/rid.guard';
+import { UserGuard } from './guards/user.guard';
+import { HealthController } from './health/health.controller';
+import { MicroserviceHealthIndicator } from './health/rmq.health';
+import { PermissionsGuard } from './permissions.guard';
+import { UsersModule } from './users/users.module';
+import { validate } from './validation/env.validation';
+import { Tokenizer } from './validation/tokenizer';
 
 @Module({
   imports: [
