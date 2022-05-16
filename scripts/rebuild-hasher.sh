4f4f87d375317940de50bf52e937fb60f77213bc
raikasdev <roni.aikas@gmail.com>
2022-05-16 22:38:39 +0300

    fix: remove rebuild hasher

--- scripts/rebuild-hasher.sh
+++
@@ -1,7 +0,0 @@
-if [ "$1" = "auth" ] || [ "$1" = "users" ] 
-then 
-    apk add --no-cache make gcc g++ python3
-    npm rebuild argon2 --build-from-source
-    apk del make gcc g++ python3
-    echo "balls" > /home/node/balls
-fi
