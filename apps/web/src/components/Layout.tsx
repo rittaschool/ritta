26941877d139b3630419a9ae6f33bf22f5831c17
Roni Äikäs <roni.aikas@gmail.com>
2022-07-13 22:58:32 +0300

    feat: new navigation

--- apps/web/src/components/Layout.tsx
+++ apps/web/src/components/Layout.tsx
@@ -4,16 +4,14 @@ import {
   Center,
   Header,
   MediaQuery,
-  Navbar,
   useMantineTheme,
+  Navbar as MantineNavbar,
 } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { ReactNode, useState } from "react";
 import { Calendar, File, GitMerge, Home } from "react-feather";
 import { Messages } from "tabler-icons-react";
-import { HeaderLogout } from "./HeaderLogout";
-import { HeaderUser } from "./HeaderUser";
-import LayoutLink from "./LayoutLink";
+import { NavbarNested as Navbar } from "./navigation/Navbar";
 import Logo from "./Logo";
 
 // Links
@@ -45,46 +43,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       navbarOffsetBreakpoint="sm"
       // fixed prop on AppShell will be automatically added to Header and Navbar
       fixed
-      navbar={
-        <Navbar
-          p="md"
-          // Breakpoint at which navbar will be hidden if hidden prop is true
-          hiddenBreakpoint="sm"
-          // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
-          hidden={!opened}
-          // when viewport size is less than theme.breakpoints.sm navbar width is 100%
-          // viewport size > theme.breakpoints.sm – width is 300px
-          // viewport size > theme.breakpoints.lg – width is 400px
-          width={{ sm: 300 }}
-        >
-          {/* First section with normal height (depends on section content) */}
-          {/* <Navbar.Section>First section</Navbar.Section> */}
-          {!isMobile ? (
-            <Navbar.Section>
-              <a href="/">
-                <Logo />
-              </a>
-            </Navbar.Section>
-          ) : (
-            <></>
-          )}
-          <Navbar.Section grow>
-            {links.map((ele) => (
-              <LayoutLink key={ele.label} {...ele} />
-            ))}
-          </Navbar.Section>
-          <Navbar.Section>
-            <span
-              style={{ display: "inline-flex", alignItems: "center" }}
-              className="legend-label"
-            >
-              <GitMerge style={{ marginRight: "5px" }} /> {__COMMIT_HASH__}
-            </span>
-            <HeaderUser />
-            <HeaderLogout />
-          </Navbar.Section>
-        </Navbar>
-      }
+      navbar={<Navbar hidden={!opened} />}
       header={
         <MediaQuery largerThan="sm" styles={{ display: "none" }}>
           <Header height={isMobile ? 70 : 0}>
@@ -101,13 +60,13 @@ export default function Layout({ children }: { children: ReactNode }) {
                 mr="xl"
               />
               {isMobile ? (
-                <Navbar.Section grow>
+                <MantineNavbar.Section grow>
                   <Center>
                     <a href="/">
                       <Logo />
                     </a>
                   </Center>
-                </Navbar.Section>
+                </MantineNavbar.Section>
               ) : (
                 <></>
               )}
---
+++ apps/web/src/components/navigation/Navbar.tsx
@@ -0,0 +1,148 @@
+import {
+  Navbar,
+  Group,
+  Code,
+  ScrollArea,
+  createStyles,
+  Center,
+  Button,
+} from "@mantine/core";
+import {
+  Notes,
+  CalendarStats,
+  Gauge,
+  PresentationAnalytics,
+  FileAnalytics,
+  Adjustments,
+  Lock,
+  Link,
+  Selector,
+  Logout,
+} from "tabler-icons-react";
+import { UserButton } from "./UserButton";
+import { LinksGroup } from "./NavbarLinksGroup";
+// @ts-ignore
+import Logo from "../../../static/logo.svg?component";
+
+const mockdata = [
+  { label: "Dashboard", icon: Gauge },
+  {
+    label: "Market news",
+    icon: Notes,
+    initiallyOpened: true,
+    links: [
+      { label: "Overview", link: "/" },
+      { label: "Forecasts", link: "/" },
+      { label: "Outlook", link: "/" },
+      { label: "Real time", link: "/" },
+    ],
+  },
+  {
+    label: "Releases",
+    icon: CalendarStats,
+    links: [
+      { label: "Upcoming releases", link: "/" },
+      { label: "Previous releases", link: "/" },
+      { label: "Releases schedule", link: "/" },
+    ],
+  },
+  { label: "Analytics", icon: PresentationAnalytics },
+  { label: "Contracts", icon: FileAnalytics },
+  { label: "Settings", icon: Adjustments },
+  {
+    label: "Security",
+    icon: Lock,
+    links: [
+      { label: "Enable 2FA", link: "/" },
+      { label: "Change password", link: "/" },
+      { label: "Recovery codes", link: "/" },
+    ],
+  },
+];
+
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
+  },
+
+  header: {
+    padding: theme.spacing.md,
+    paddingTop: 0,
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+  },
+
+  links: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+  },
+
+  user: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    marginBottom: theme.spacing.md,
+    borderBottom: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  logOut: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    paddingTop: theme.spacing.md,
+    paddingBottom: theme.spacing.md,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+}));
+
+export function NavbarNested({ hidden }: { hidden: boolean }) {
+  const { classes } = useStyles();
+  const links = mockdata.map((item) => (
+    <LinksGroup {...item} key={item.label} />
+  ));
+
+  return (
+    <Navbar
+      width={{ sm: 300 }}
+      pt="md"
+      px="md"
+      className={classes.navbar}
+      hidden={hidden}
+    >
+      <Navbar.Section className={classes.header}>
+        <Group position="apart">
+          <Logo width={120} />
+          <Code sx={{ fontWeight: 700 }}>
+            {import.meta.env.DEV ? "DEV " : "PROD "} {__COMMIT_HASH__}
+          </Code>
+        </Group>
+      </Navbar.Section>
+
+      <Navbar.Section className={classes.user}>
+        <UserButton
+          image="https://i.imgur.com/fGxgcDF.png"
+          name="Olli Opettaja"
+          title="Opettaja"
+          schoolName="Rittalan yhteiskoulu"
+          icon={<Selector size={14} />}
+        />
+      </Navbar.Section>
+
+      <Navbar.Section grow className={classes.links} component={ScrollArea}>
+        {links}
+      </Navbar.Section>
+      <Navbar.Section className={classes.logOut}>
+        <Center>
+          <Button variant="light" color="red" sx={{ width: "80%" }}>
+            Kirjaudu ulos <Logout style={{ marginLeft: "5px" }} />
+          </Button>
+        </Center>
+      </Navbar.Section>
+    </Navbar>
+  );
+}
