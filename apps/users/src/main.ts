9bca4fdc11a24de9a1af246f8984f9e19c25c986
Midka <me@midka.dev>
2022-07-14 19:20:57 +0300

    feat(framework): migrate users to framework

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,42 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { config } from 'dotenv';
-import { WinstonModule } from 'nest-winston';
-import { transports } from 'winston';
+import { createService } from 'framework';
 import { AppModule } from './app.module';
-import { consoleFormat } from './logger.format';
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
+  const app = await createService({
+    name: 'messages',
+    queue: {
+      name: 'messages',
+      url: `amqp://${process.env.RMQ_PASSWORD}:${process.env.RMQ_USERNAME}@${process.env.RMQ_HOST}:${process.env.RMQ_PORT}/`,
       options: {
-        urls: [
-          `amqp://${process.env.RMQ_PASSWORD}:${process.env.RMQ_USERNAME}@${process.env.RMQ_HOST}:${process.env.RMQ_PORT}/`,
-        ],
-        queue: 'users',
-        queueOptions: {
-          durable: true,
-        },
+        durable: true,
       },
-      logger: WinstonModule.createLogger({
-        defaultMeta: { service: 'users', enviroment: process.env.NODE_ENV },
-        transports: [
-          new transports.Console({
-            format: consoleFormat,
-          }),
-          new transports.File({ filename: 'logs/all.log' }),
-        ],
-      }),
     },
-  );
-
-  await app.listen().then(() => {
-    app
-      .get('LOGGER')
-      .log({ message: `Users service is online`, context: 'Main' });
+    appModule: AppModule,
   });
+
+  app.start();
 }
 bootstrap();
