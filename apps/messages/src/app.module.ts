8d345c88accc71cb37a874aca206a2bcbbd7ead7
Midka <me@midka.dev>
2022-07-15 21:57:41 +0300

    fix: use common module from framework in messages

--- apps/messages/src/app.module.ts
+++ apps/messages/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { validate } from './validation/env.validation';
-import { MessagesModule } from './messages/messages.module';
-import { AnnouncementsModule } from './announcements/announcements.module';
-import { CommonModule } from './common/common.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from 'framework';
+import { AnnouncementsModule } from './announcements/announcements.module';
+import { MessagesModule } from './messages/messages.module';
 import { MongooseConfigService } from './mongoose';
+import { validate } from './validation/env.validation';
 
 @Module({
   imports: [
