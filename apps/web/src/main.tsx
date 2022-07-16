708f4513f001a3414385723549906344ca1eac5e
Roni Äikäs <roni.aikas@gmail.com>
2022-07-16 22:23:11 +0300

    chore: merge

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,73 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import './styles/index.css';
-
-// Init languages
-import './i18n';
-
-// Import templates
-import Admin from './templates/Admin';
-import Auth from './templates/Auth';
-import Dashboard from './templates/Dashboard';
-
-// Pages
-<<<<<<< HEAD
-import Home from "./pages/Home";
-import MessagesList, { MailBox } from "./pages/MessagesList";
-import Login from "./pages/auth/Login";
-import LogOut from "./pages/auth/LogOut";
-import AdminHome from "./pages/admin/Home";
-import OldLogin from "./pages/auth/OldLogin";
-import CreateMessage from "./pages/CreateMessage";
-import ThreadView from "./pages/ThreadView";
-=======
-import { QueryClient, QueryClientProvider } from 'react-query';
-import AdminHome from './pages/admin/Home';
-import Login from './pages/auth/Login';
-import LogOut from './pages/auth/LogOut';
-import CreateMessage from './pages/CreateMessage';
-import Home from './pages/Home';
-import MessagesList, { MailBox } from './pages/MessagesList';
->>>>>>> 5f6817a0e5798f133d6a11bc50b068ce79655751
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./styles/index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Init languages
+import "./i18n";
+
+// Import templates
+import Admin from "./templates/Admin";
+import Auth from "./templates/Auth";
+import Dashboard from "./templates/Dashboard";
+
+// Pages
+import AdminHome from "./pages/admin/Home";
+import Login from "./pages/auth/Login";
+import LogOut from "./pages/auth/LogOut";
+import CreateMessage from "./pages/CreateMessage";
+import Home from "./pages/Home";
+import MessagesList, { MailBox } from "./pages/MessagesList";
+import ThreadView from "./pages/ThreadView";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />}>
-          <Route index element={<Home />} />
-          <Route path="messages">
-            <Route path=":id" element={<ThreadView />} />
-            <Route path="sent" element={<MessagesList box={MailBox.SENT} />} />
-            <Route
-              path="drafts"
-              element={<MessagesList box={MailBox.DRAFTS} />}
-            />
-            <Route
-              path="archive"
-              element={<MessagesList box={MailBox.ARCHIVE} />}
-            />
-            <Route path="new" element={<CreateMessage />} />
-            <Route index element={<MessagesList box={MailBox.INBOX} />} />
-          </Route>
-        </Route>
-        <Route path="/auth" element={<Auth />}>
-          <Route index element={<Navigate to="/auth/login" />} />
-          <Route path="login" element={<Login />} />
-          <Route path="logout" element={<LogOut />} />
-          <Route path="old" element={<OldLogin />} />
-          {/* <Route path="teams" element={<Teams />}>
-=======
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />}>
             <Route index element={<Home />} />
             <Route path="messages">
+              <Route path=":id" element={<ThreadView />} />
               <Route
                 path="sent"
                 element={<MessagesList box={MailBox.SENT} />}
@@ -83,18 +47,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               <Route path="new" element={<CreateMessage />} />
               <Route index element={<MessagesList box={MailBox.INBOX} />} />
             </Route>
-            {/* <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route> */}
           </Route>
           <Route path="/auth" element={<Auth />}>
             <Route index element={<Navigate to="/auth/login" />} />
             <Route path="login" element={<Login />} />
             <Route path="logout" element={<LogOut />} />
             {/* <Route path="teams" element={<Teams />}>
->>>>>>> 5f6817a0e5798f133d6a11bc50b068ce79655751
             <Route path=":teamId" element={<Team />} />
             <Route path="new" element={<NewTeamForm />} />
             <Route index element={<LeagueStandings />} />
