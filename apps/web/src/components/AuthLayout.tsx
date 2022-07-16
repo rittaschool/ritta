220d58d45ccf39c0d74b2e04463edc3a12c14ced
Midka <me@midka.dev>
2022-07-16 19:15:57 +0300

    feat(auth): add email validation

--- apps/web/src/components/AuthLayout.tsx
+++ apps/web/src/components/AuthLayout.tsx
@@ -1,23 +1,18 @@
 import {
-  TextInput,
-  PasswordInput,
   Anchor,
-  Paper,
-  Title,
-  Text,
-  Container,
-  Group,
-  Button,
   Badge,
-  Stack,
-  Space,
+  Container,
+  Paper,
   Select,
-} from "@mantine/core";
-import { useTranslation } from "react-i18next";
-import Logo from "./Logo";
-import Languages from "../../public/locales/locales.json";
-import { ReactElement, useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+  Space,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Languages from '../../public/locales/locales.json';
+import Logo from './Logo';
 
 export default function App({ children }: { children: JSX.Element }) {
   const languages: {
@@ -25,42 +20,42 @@ export default function App({ children }: { children: JSX.Element }) {
   } = Languages;
 
   const { t, i18n } = useTranslation();
-  const [lang, setLang] = useState("");
+  const [lang, setLang] = useState('');
 
   useEffect(() => {
-    if (lang || lang !== "") {
+    if (lang || lang !== '') {
       i18n.changeLanguage(lang);
     }
   }, [lang]);
 
   return (
     <>
-      <Container size={420} my={40}>
+      <Container size={500} my={40}>
         <Title
           align="center"
           sx={(theme) => ({
             fontFamily: `Greycliff CF, ${theme.fontFamily}`,
             fontWeight: 900,
-            marginBottom: "25px",
+            marginBottom: '25px',
           })}
         >
           <Stack spacing="sm">
             <Logo center={true} />
             <Badge color="teal" size="xl" variant="filled">
-              {t("auth:stage")}
+              {t('auth:stage')}
             </Badge>
           </Stack>
         </Title>
         <Text color="dimmed" size="sm" align="center" mt={5}>
-          {t("auth:info.operated", { name: "Rittalan yhtenäiskoulu" })}
+          {t('auth:info.operated', { name: 'Rittalan yhtenäiskoulu' })}
           <Space />
-          {t("auth:info.for_support")}{" "}
-          <Anchor<"a">
+          {t('auth:info.for_support')}{' '}
+          <Anchor<'a'>
             href="https://ritta.fi"
             size="sm"
             onClick={(event) => event.preventDefault()}
           >
-            {t("auth:info.support_center")}
+            {t('auth:info.support_center')}
           </Anchor>
         </Text>
 
@@ -68,16 +63,16 @@ export default function App({ children }: { children: JSX.Element }) {
           {children}
         </Paper>
       </Container>
-      <Container size={420} my={20}>
+      <Container size={500} my={20}>
         <Paper withBorder shadow="md" p={30} pt={25} mt={30} radius="md">
           <Select
-            label={"Change language"}
+            label={'Change language'}
             value={lang || i18n.language}
             data={Object.keys(languages).map((languageId) => ({
               value: languageId,
               label: languages[languageId],
             }))}
-            onChange={(lang) => setLang(lang || "fi")}
+            onChange={(lang) => setLang(lang || 'fi')}
           />
         </Paper>
       </Container>
--- apps/web/src/pages/auth/Login.tsx
+++ apps/web/src/pages/auth/Login.tsx
@@ -1,58 +1,54 @@
-import {
-  Group,
-  TextInput,
-  Button,
-  PasswordInput,
-  Anchor,
-  Divider,
-  Grid,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { useTranslation } from "react-i18next";
-import { GoogleButton } from "../../components/SocialButtons";
+import { Avatar, Button, Group, Stepper, TextInput } from '@mantine/core';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { EmailRegEx } from '../../utils/email.regex';
 
 const Login = () => {
-  const form = useForm({
-    initialValues: {
-      username: "",
-      password: "",
-    },
-  });
-
+  const [active, setActive] = useState(1);
+  const nextStep = () =>
+    setActive((current) => (current < 2 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) => (current > 0 ? current - 1 : current));
   const { t } = useTranslation();
+  const [email, setEmail] = useState('');
 
   return (
-    <form onSubmit={form.onSubmit((values) => console.log(values))}>
-      <TextInput
-        label={t("auth:username")}
-        required
-        {...form.getInputProps("username")}
-      />
-      <PasswordInput
-        label={t("auth:password")}
-        required
-        mt="md"
-        {...form.getInputProps("password")}
-      />
-      <Group position="apart" mt="md">
-        <Anchor<"a">
-          onClick={(event) => event.preventDefault()}
-          href="/auth/forgot"
-          size="sm"
+    <>
+      <Stepper active={active} onStepClick={setActive} breakpoint="sm">
+        <Stepper.Step
+          label="Step 1"
+          description={t('auth:username')}
+          allowStepSelect={active > 0}
         >
-          {t("auth:forgot_password")}
-        </Anchor>
+          <TextInput
+            required
+            label={t('auth:username')}
+            placeholder="etunimi.sukunimi@ritta.app"
+            value={email}
+            onChange={(event) => setEmail(event.currentTarget.value)}
+            error={!EmailRegEx.test(email) && email && 'Invalid email'}
+          />
+        </Stepper.Step>
+        <Stepper.Step
+          label="Step 2"
+          description="Authenticate"
+          allowStepSelect={active > 1}
+        >
+          <Avatar alt="your profile picture" />
+          Authenticate with password or security key or push notification
+        </Stepper.Step>
+        <Stepper.Completed>
+          Successfully logged in. Continue to dashboard
+        </Stepper.Completed>
+      </Stepper>
+
+      <Group position="center" mt="xl">
+        <Button variant="default" onClick={prevStep}>
+          Back
+        </Button>
+        <Button onClick={nextStep}>Continue</Button>
       </Group>
-      <Button fullWidth mt="xl" type="submit">
-        {t("auth:login")}
-      </Button>
-      <Divider label={t("auth:or_use")} labelPosition="center" my="lg" />
-      <Grid grow mb="md" mt="md" gutter="md">
-        <Grid.Col span={2}>
-          <GoogleButton radius="xl">Google</GoogleButton>
-        </Grid.Col>
-      </Grid>
-    </form>
+    </>
   );
 };
 
