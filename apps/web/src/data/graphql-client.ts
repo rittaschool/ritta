e9c4376f92c88bbc4699202830ba6e03eec59073
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-08-02 22:48:04 +0300

    refactor: rename authToken to accessToken

--- apps/web/src/data/graphql-client.ts
+++ apps/web/src/data/graphql-client.ts
@@ -2,8 +2,8 @@ import { GraphQLClient } from 'graphql-request';
 
 const ENDPOINT = 'http://localhost:3000/graphql';
 
-const authToken = sessionStorage.getItem('access_token');
+const accessToken = sessionStorage.getItem('access_token');
 
 export const client = new GraphQLClient(ENDPOINT, {
-  headers: authToken ? { authorization: `Bearer ${authToken}` } : {},
+  headers: accessToken ? { authorization: `Bearer ${accessToken}` } : {},
 });
