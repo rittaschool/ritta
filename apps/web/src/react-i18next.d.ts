21cc9b526e79697a1202d51b66dcecbd48b360f7
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-08-03 01:04:48 +0300

    refactor: remove unnecessary i18n import (#133)

--- apps/web/src/react-i18next.d.ts
+++ apps/web/src/react-i18next.d.ts
@@ -1,11 +1,10 @@
-import { translationResources } from "./i18n";
 import commonTranslations from "../public/locales/en/common.json";
 import authTranslations from "../public/locales/en/auth.json";
 import homeTranslations from "../public/locales/en/home.json";
 import messagesTranslations from "../public/locales/en/messages.json";
 import navigationTranslations from "../public/locales/en/navigation.json";
 
-export const translationResources = {
+const translationResources = {
   common: commonTranslations,
   auth: authTranslations,
   home: homeTranslations,
