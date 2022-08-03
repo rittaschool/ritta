f362f2df494000b88842849d971caed26d7a4540
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-08-03 23:58:14 +0300

    refactor(schedule): remove infinite loop

--- apps/web/src/utils/scheduleUtils.ts
+++ apps/web/src/utils/scheduleUtils.ts
@@ -3,25 +3,23 @@ import { Lesson } from "../components/schedule/SingleScheduleEntry";
 export const getColumns = (lessons: Lesson[]) => {
   const sortedLessons = [...lessons].sort((a, b) => a.startTime.getTime() - b.startTime.getTime());
 
-  const result = sortedLessons.reduce<(Lesson & { column: number })[]>((acc, lesson) => {
-    let currentCheckColumn = 0;
+  return sortedLessons.reduce<(Lesson & { column: number })[]>((insertedLessons, lesson) => {
+    const maxColumn = insertedLessons.length === 0 ? 0 : Math.max(...insertedLessons.map(l => l.column));
+    const columnCount = maxColumn + 1;
 
-    while (true) {
-      const canFitInColumn = acc
-        .filter(r => r.column === currentCheckColumn)
-        .every(r => r.endTime.getTime() < lesson.startTime.getTime());
+    for (let currentCheckColumn = 0; currentCheckColumn < columnCount + 1; currentCheckColumn++) {
+      const canFitInColumn = insertedLessons
+        .filter(l => l.column === currentCheckColumn)
+        .every(l => l.endTime.getTime() < lesson.startTime.getTime());
 
       if (canFitInColumn) {
-        return [
-          ...acc,
-          { ...lesson, column: currentCheckColumn }
-        ];
+        return insertedLessons.concat({ ...lesson, column: currentCheckColumn });
       }
-
-      currentCheckColumn++;
     }
-  }, []);
 
-  return result;
+    // This should never happen
+    console.error("Could not find a column for lesson", lesson);
+    return insertedLessons;
+  }, []);
 };
 
