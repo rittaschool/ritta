23d25708cd8ee7a3e2160ee6fffd3b2babaa685c
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-08-04 05:21:43 +0300

    feat(i18n): translate admin home page title

--- apps/web/src/react-i18next.d.ts
+++ apps/web/src/react-i18next.d.ts
@@ -3,13 +3,15 @@ import authTranslations from "../public/locales/en/auth.json";
 import homeTranslations from "../public/locales/en/home.json";
 import messagesTranslations from "../public/locales/en/messages.json";
 import navigationTranslations from "../public/locales/en/navigation.json";
+import adminTranslations from "../public/locales/en/admin.json";
 
 const translationResources = {
   common: commonTranslations,
   auth: authTranslations,
   home: homeTranslations,
   messages: messagesTranslations,
-  navigation: navigationTranslations
+  navigation: navigationTranslations,
+  admin: adminTranslations,
 }
 
 declare module "react-i18next" {
